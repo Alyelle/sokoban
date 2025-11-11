d16b2d8d2b4f30fc204eb6ed9f3d92658c80e70a
Alyelle <67128109+Alyelle@users.noreply.github.com>
2025-11-10 17:50:15 -0800

    button

---
+++ Assets/Scripts/MY CODE/ButtonTrigger.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class ButtonTrigger
+{
+    public OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("block"))
+        {
+            
+        }
+    }
+}
